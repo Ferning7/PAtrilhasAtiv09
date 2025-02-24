f42a1d4fb02f45406f608e6e0004624d2853329a
Matheus <81193702+Ferning7@users.noreply.github.com>
2025-02-24 08:55:22 -0300

    Add files via upload

---
+++ Lista 13/ex8/ex8/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ex8
+{
+	public class Triangulo
+	{
+		public double lado1 { get; set; }
+		public double lado2 { get; set; }
+		public double lado3 { get; set; }
+
+		// Constructor
+		public Triangulo(double lado1, double lado2, double lado3)
+		{
+			this.lado1 = lado1;
+			this.lado2 = lado2;
+			this.lado3 = lado3;
+		}
+
+		// Verifica o tipo do triângulo
+		public string VerificaTriangulo()
+		{
+			if (lado1 == lado2 && lado2 == lado3)
+			{
+				return "É um triângulo equilátero.";
+			}
+			else if (lado1 == lado2 && lado1 != lado3)
+			{
+				return "É um triângulo isósceles.";;
+			}
+			else
+			{
+				return "É um triângulo escaleno.";
+			}
+		}
+	}
+
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Digite o valor do primeiro lado do triângulo: ");
+			double lado1 = double.Parse(Console.ReadLine());
+			
+			Console.Write("Digite o valor do segundo lado: ");
+			double lado2 = double.Parse(Console.ReadLine());
+			
+			Console.Write("Digite o valor do terceiro lado: ");
+			double lado3 = double.Parse(Console.ReadLine());
+			
+			Triangulo triangulo = new Triangulo(lado1, lado2, lado3);
+
+			string resultado = triangulo.VerificaTriangulo();
+			Console.WriteLine(resultado);
+		}
+	}
+}
---
+++ Lista 13/ex9/ex9/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace ex9
+{
+	public class Agenda
+	{
+		List<string> nomes = new List<string>();
+		List<string> numeros = new List<string>();
+
+		// Constructor da Classe Agenda
+		public Agenda(List<string> nome, List<string> numero)
+		{
+			this.nomes = nome;
+			this.numeros = numero;
+		}
+
+		// Método que adiciona os dados do contato
+		public void AdicionarContato()
+		{
+			Console.Write("Digite o nome do contato: ");
+			string nome = Console.ReadLine().ToLower();
+			nomes.Add(nome);
+			
+			Console.Write("Digite o número de telefone do contato: ");
+			string numero = Console.ReadLine();
+			numeros.Add(numero);
+			
+			Console.WriteLine("Contato adicionado com sucesso!");
+		}
+
+		// Método que remove o contato
+		public void RemoverContato()
+		{
+			Console.Write("Digite o nome do contato: ");
+			string nome = Console.ReadLine().ToLower();
+			
+			for (int i = 0; i < nomes.Count; i++)
+			{
+				if (nomes[i] == nome)
+				{
+					nomes.RemoveAt(i);
+					numeros.RemoveAt(i);
+					Console.WriteLine("Contato removido com sucesso!");
+					return;
+				}
+			}
+		}
+
+		// Método que lista os contatos cadastrados
+		public void ListarContatos()
+		{
+			Console.WriteLine("Lista de contatos: ");
+			for (int i = 0; i < nomes.Count; i++)
+			{
+				Console.WriteLine(nomes[i] + " - " + numeros[i]);
+			}
+		}
+		
+		public void ExibirMenu()
+		{
+			while (true)
+			{
+				Console.Clear();
+				Console.WriteLine("===Agenda===");
+				Console.WriteLine("Escolha uma operação:");
+				Console.WriteLine("1 - Adicionar Contato");
+				Console.WriteLine("2 - Remover Contato");
+				Console.WriteLine("3 - Listar Contatos");
+				Console.WriteLine("4 - Sair");
+				Console.Write("Opção: ");
+				
+				int opcao = int.Parse(Console.ReadLine());
+
+				switch (opcao)
+				{
+					case 1:
+						AdicionarContato();
+						break;
+					case 2:
+						RemoverContato();
+						break;
+					case 3:
+						ListarContatos();
+						break;
+					case 4:
+						Console.WriteLine("Saindo...");
+						return;
+					default:
+						Console.WriteLine("Opção Inválida.");
+						break;
+				}
+				
+				// Aguarda o usuário pressionar uma tecla para continuar
+				Console.WriteLine("Pressione qualquer tecla para continuar...");
+				Console.ReadKey(true);
+			}
+		}
+	}
+
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Agenda agenda = new Agenda(new List<string>(), new List<string>());
+			agenda.ExibirMenu();
+		}
+		
+	}
+}
